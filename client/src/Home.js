0631558fa9e86a70c877a097fd1e389f9489a57a
Cosmas Gikunju <gikunjucosmas@gmail.com>
2020-05-20 22:46:44 +0300

    Client: Better notes on the Homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -15,14 +15,36 @@ function Home() {
 					<h4 className='text text--white'>
 						Tip: Try Give Some Clear Images
 					</h4>
+					<h4 className='text text--white'>
+						The Model was trained using a German traffic signs
+						dataset of select 42 different traffic signs in Germany.
+					</h4>
+					<h4 className='text text--white'>
+						Note: The backend is powered by a free heroku dyno which
+						sleeps often and takes time to boot
+					</h4>
 				</div>
 				<Classifier />
 				<div className='footer'>
 					<p className='text text--grey'>
-						The Model Accuracy score against training data is:{' '}
+						The Model Accuracy score against test data is:{' '}
 						<span className='text--bold'>0.97537</span>
 					</p>
 					<p className='text text--grey'>
+						You can extend this model's perfomance with more
+						datasets for better prediction accuracy.
+					</p>
+					<p className='text text--grey'>
+						The Training Source code is available here:{' '}
+						<a
+							href={`https://github.com/ItsCosmas/Traffic-Sign-Classification`}
+							rel='noopener noreferrer'>
+							https://github.com/ItsCosmas/Traffic-Sign-Classification
+						</a>
+					</p>
+					<p className='text text--grey'>
+						The Deployment source code for this page and the backend
+						is available here:{' '}
 						<a
 							href={`https://github.com/ItsCosmas/traffic-signs-classifier`}
 							rel='noopener noreferrer'>
